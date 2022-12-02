274c79cedb6b69a13dddc188e1f085f14a530eb7
Ramazanov Djabrail <ramazanovd612@gmail.com>
2022-12-02 21:36:25 +0600

    Создан компонентс пост

---
+++ src/components/Post/Posts.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Posts = () => {
+  return (
+    <div>
+      popopop
+    </div>
+  )
+}
+
+export default Posts
--- src/container/Home/Home.tsx
+++ src/container/Home/Home.tsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axiosApi from '../../AxiosApi'
+import { Post } from '../../types'
 
 const Home = () => {
+  const [posts, setPosts] = useState<Post[] | null>(null)
+  const [response , setResponse] = useState(null)
+  const requestPosts = async () => {
+    const request = await axiosApi.get('/orders.json')
+    setPosts(request.data)
+    setResponse((
+      
+    ))
+  }
+
   return (
     <div>
       Home
